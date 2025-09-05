e31c14d8d4f278b3a0d681860666b72a55858928
David <david.nossebro@gmail.com>
2025-09-05 13:55:47 +0200

    Fix npm install args - use proper npm global install approach

--- Formula/gift-calc.rb
+++ Formula/gift-calc.rb
@@ -9,7 +9,7 @@ class GiftCalc < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *std_npm_args(libexec)
+    system "npm", "install", "-g", "--prefix=#{libexec}"
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
