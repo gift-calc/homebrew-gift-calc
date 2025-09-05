b1b362980db53a7cc6916357efbd3b1d6cab350e
David <david.nossebro@gmail.com>
2025-09-05 13:50:37 +0200

    Fix SHA256 format - use proper string hash instead of symbol

--- Formula/gift-calc.rb
+++ Formula/gift-calc.rb
@@ -3,7 +3,7 @@ class GiftCalc < Formula
   homepage "https://github.com/gift-calc/gift-calc"
   url "https://registry.npmjs.org/gift-calc/-/gift-calc-1.2.1.tgz"
   version "1.2.1"
-  sha256 :no_check
+  sha256 "a3725ba1e59652b09dada40dab50182d91a47a0f16f90f8712a6c37b15d5d099"
   license "ISC"
 
   depends_on "node"
