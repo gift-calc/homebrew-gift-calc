8869eee5e10f800ef329a9af469a3a9d4d8812fb
GitHub Action <action@github.com>
2025-09-12 14:19:47 +0000

    Update gift-calc to version 2.5.0

--- Formula/gift-calc.rb
+++ Formula/gift-calc.rb
@@ -1,9 +1,9 @@
 class GiftCalc < Formula
   desc "CLI tool that calculates gift amounts with configurable randomness"
   homepage "https://github.com/gift-calc/gift-calc"
-  url "https://registry.npmjs.org/gift-calc/-/gift-calc-2.4.1.tgz"
-  version "2.4.1"
-  sha256 "dcce79225f5ef0f241b465c4b484933164090fd4b96b6925fc98f6c7df76b5e4"
+  url "https://registry.npmjs.org/gift-calc/-/gift-calc-2.5.0.tgz"
+  version "2.5.0"
+  sha256 "f7a751736494c8749bb54462679e1503140d3be9808cbbdf54477a66530eb126"
   license "ISC"
 
   depends_on "node"
